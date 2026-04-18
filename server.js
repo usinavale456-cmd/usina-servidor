621bafc0650444b9c84e23146fcb1168feebae18
cidinho <rodriguesdodo@hotmail.com>
2026-04-18 17:31:38 -0300

    Update port for Vercel deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.static(path.join(__dirname, 'public')));
